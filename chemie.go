d28a3326d64dbfbd75bd8a78a51464a813938ee3
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-11-10 21:48:18 +0100

    napsano debug

--- chemie.go
+++ chemie.go
@@ -9,6 +9,7 @@ type day struct {
 }
 
 func main() {
+	// vstup dat
 	var N, D int
 	fmt.Scanf("%d%d", &N, &D)
 	days := make([]day, D)
@@ -33,4 +34,51 @@ func main() {
 			}
 		}
 	}
+	// vlastní počítání
+	var byCost, byDur []day
+	var cost int
+	byCost = make([]day, 0)
+	byDur = make([]day, 0)
+	for i := 1; i <= N; i++ { // proiteruje kazdy den
+		if days[0].start == i { // přidání lahviček
+			days[0].dur += (days[0].start + 1)
+			byDur = append(byDur, days[0]) // přidání dle výdrže
+			for j := len(byDur) - 1; j <= 0; j-- {
+				if byDur[j-1].dur < byDur[j].dur {
+					swp := byDur[j-1]
+					byDur[j-1] = days[j]
+					days[j] = swp
+				}
+			}
+			byCost = append(byCost, days[0]) // přidání dle ceny
+			for j := len(byCost) - 1; j <= 0; j-- {
+				if byCost[j-1].cost < byCost[j].cost {
+					swp := byCost[j-1]
+					byCost[j-1] = days[j]
+					days[j] = swp
+				}
+			}
+			days = days[1:]
+		}
+		for byDur[0].dur == i { // odstraneni proslych lahvicek
+			var tmp day
+			for j := 0; j < len(byCost); j++ {
+				if byCost[j] == byDur[0] {
+					continue
+				} else {
+					swp := tmp
+					tmp = days[j]
+					days[j] = swp
+				}
+			}
+			byCost = byCost[:len(byCost)-1]
+			byDur = byDur[1:]
+		}
+		if len(byCost) == 0 {
+			fmt.Printf("Experiment konci dnem %d\n", i)
+		} else {
+			cost += byCost[0].cost
+		}
+	}
+	fmt.Printf("%d\n", cost)
 }
